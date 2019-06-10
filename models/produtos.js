584b8c68123527d066e9468865e82d4402f3fb04
marciock <marciockalves@gmail.com>
2019-06-10 05:51:23 -0300

    barcode

---
+++ models/produtos.js
@@ -0,0 +1,19 @@
+const produtos=[
+    {
+      id:'1',
+      produto:'batata',
+      valor:'3,00'
+    },
+    {
+      id:'2',
+      produto:'feijao',
+      valor:'5,00'
+    },
+    {
+      id:'3',
+      produto:'bolacha',
+      valor:'1,70'
+    },
+  ];
+
+module.exports=produtos;
